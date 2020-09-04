56fa1e4e90326298aa20d2ff7bb95503279ce074
SamuelJTaylor22 <sjtaylor1998@gmail.com>
2020-09-04 15:20:06 -0600

    added clock

--- app/Controllers/ImageController.js
+++ app/Controllers/ImageController.js
@@ -9,11 +9,22 @@ function _drawbg(){
   document.getElementById("main").style.backgroundImage = `url('${ProxyState.image.url}')`
 }
 
+function _drawClock(){
+  let today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes().toString();
+  if(parseInt(m) < 10){ m = "0"+ m}
+  
+  setTimeout(_drawClock, 500);
+  document.getElementById("time").innerText = h + ":" + m;
+}
+
 export default class ImageController {
   constructor(){
     
     ProxyState.on("image", _drawbg )
     imageService.getimage()
+    _drawClock()
   }
 
 
